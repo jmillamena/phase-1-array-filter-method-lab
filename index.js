c359ef41a3ca2b6ee9dc3e6c6a873e158d76032e
Jmillamena <jcmillamena@gmail.com>
2023-08-04 12:12:25 -1000

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(driverArray, name) {
+  const nameSearch = driverArray.filter((driver) => {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+  return nameSearch;
+}
+
+function fuzzyMatch(driversArray, name) {
+  const searchLetter = driversArray.filter((driver) => {
+    return driver[0] === name[0];
+  });
+  return searchLetter;
+}
+
+function matchName(driverObjects, string) {
+  const searchObjects = driverObjects.filter((objects) => {
+    return objects.name === string;
+  });
+  return searchObjects;
+}
